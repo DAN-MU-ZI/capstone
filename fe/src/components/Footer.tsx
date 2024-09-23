ba01810eaf2e69b895092b58949ffa2a2e3e2910
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-09-23 19:00:10 +0900

    푸터 테마 적용

--- fe/src/components/Footer.tsx
+++ fe/src/components/Footer.tsx
@@ -1,9 +1,13 @@
+// Footer.tsx
 import React from 'react';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="p-4 bg-gray-800 text-white mt-8">
-            <p>© 2024 My Website. All rights reserved.</p>
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                {/* © 2024 My Website. All rights reserved. */}
+                <p className="text-center md:text-left mb-4 md:mb-0">© 2024 My Website. All rights reserved.</p>
+            </div>
         </footer>
     );
 }
